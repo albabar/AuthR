9380dbfc4f65f4913dd62fbcd8ad11178e4cca8c
Babar <knock@babar.im>
2015-05-17 13:10:56 +0600

    Add inspire helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,13 @@
 module PagesHelper
+  def inspire
+    quote = [
+        'When there is no desire, all things are at peace. - Laozi',
+        'Simplicity is the ultimate sophistication. - Leonardo da Vinci',
+        'Simplicity is the essence of happiness. - Cedric Bledsoe',
+        'Smile, breathe, and go slowly. - Thich Nhat Hanh',
+        'Simplicity is an aquired taste. - Katharine Gerould',
+    ]
+
+    quote[rand(quote.length)]
+  end
 end
