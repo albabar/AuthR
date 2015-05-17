73d25aa36482359f0ff7189a9b760291b444bfe3
Babar <knock@babar.im>
2015-05-18 01:19:42 +0600

    Add model method n controller for oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,18 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:twitter]
+
+  # oAuth user, now only works with Twitter
+  def self.find_form_oauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.access_token = auth.access_token
+      user.twitter_secret = auth.twitter_secret
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
